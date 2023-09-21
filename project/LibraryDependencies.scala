7ade130cb999672e2ec1aae14517f33a61fc42e4
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-09-21 10:18:00 +0100

    API-7355 - Drop libs that come transitively

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -4,15 +4,13 @@ object LibraryDependencies {
   def apply() = compileDependencies ++ testDependencies
 
   lazy val compileDependencies = Seq(
-    "com.typesafe.play"       %% "play-json"                      % "2.9.2",
-    "uk.gov.hmrc"             %% "api-platform-common-domain"     % "0.3.0",
-    "uk.gov.hmrc"             %% "play-json-union-formatter"      % "1.18.0-play-28"
+    "uk.gov.hmrc"             %% "api-platform-common-domain"     % "0.3.0"
   )
 
   lazy val testDependencies = Seq(
     "org.scalactic"           %% "scalactic"                      % "3.2.14"            % "test",
     "org.scalatest"           %% "scalatest"                      % "3.2.14"            % "test",
     "com.vladsch.flexmark"     % "flexmark-all"                   % "0.62.2"            % "test",
-    "org.mockito"             %% "mockito-scala-scalatest"        % "1.7.1"             % "test"
+    "org.mockito"             %% "mockito-scala-scalatest"        % "1.17.22"             % "test"
   )
 }
