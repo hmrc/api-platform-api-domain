e847e928321267332447edbc1d464303f79b0e6b
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-10-02 12:01:15 +0100

    IMPROVEMENT - pr comment

--- src/main/scala/uk/gov/hmrc/apiplatform/modules/apis/domain/models/HttpMethod.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/modules/apis/domain/models/HttpMethod.scala
@@ -33,7 +33,7 @@ object HttpMethod {
 
   def apply(text: String): Option[HttpMethod] = HttpMethod.values.find(_.toString == text.toUpperCase)
 
-  def unsafeApply(text: String): HttpMethod = apply(text).getOrElse(throw new RuntimeException(s"$text is not a valid API Http Method"))
+  def unsafeApply(text: String): HttpMethod = apply(text).getOrElse(throw new RuntimeException(s"$text is not a valid HTTP Method"))
 
-  implicit val format = SealedTraitJsonFormatting.createFormatFor[HttpMethod]("API Http Method", apply)
+  implicit val format = SealedTraitJsonFormatting.createFormatFor[HttpMethod]("HTTP Method", apply)
 }
