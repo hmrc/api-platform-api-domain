c9ff97abfe0664fdef3f7ddf8906705345ee4e7c
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-12-15 11:46:05 +0000

    APIS-6481 - Use test common lib

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -5,13 +5,14 @@ object LibraryDependencies {
   def apply() = compileDependencies ++ testDependencies
 
   lazy val compileDependencies = Seq(
-    "uk.gov.hmrc"             %% "api-platform-common-domain"     % "0.7.0"
+    "uk.gov.hmrc"             %% "api-platform-common-domain"       % "0.8.0"
   )
 
   lazy val testDependencies = Seq(
-    "com.vladsch.flexmark"     % "flexmark-all"                   % "0.62.2",
-    "org.mockito"             %% "mockito-scala-scalatest"        % "1.17.29",
-    "org.scalactic"           %% "scalactic"                      % scalatestVersion,
-    "org.scalatest"           %% "scalatest"                      % scalatestVersion
+    "com.vladsch.flexmark"     % "flexmark-all"                     % "0.62.2",
+    "org.mockito"             %% "mockito-scala-scalatest"          % "1.17.29",
+    "org.scalactic"           %% "scalactic"                        % scalatestVersion,
+    "org.scalatest"           %% "scalatest"                        % scalatestVersion,
+    "uk.gov.hmrc"             %% "api-platform-test-common-domain"  % "0.8.0",
     ).map(_ % "test")
 }
