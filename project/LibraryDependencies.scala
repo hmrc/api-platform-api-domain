d45c1d0ce2368159189c758c5ee6a73f74fb9da8
John Green <john.green@digital.hmrc.gov.uk>
2024-07-23 09:29:56 +0100

    APIS-7219: Bump common domain library

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object LibraryDependencies {
   val scalatestVersion = "3.2.17"
-  val commonDomainVersion = "0.13.0"
+  val commonDomainVersion = "0.15.0"
 
   lazy val commonDomain = compileDependencies ++ testDependencies.map(_ % "test")
 
