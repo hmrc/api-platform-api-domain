6f75e12306de484d56d0029319295b06903ddc2f
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-03-18 08:08:20 +0000

    API-8992 - Bump common domain version

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -1,22 +1,22 @@
 import sbt._
 
 object LibraryDependencies {
-  val scalatestVersion = "3.2.17"
-  val commonDomainVersion = "0.19.0"
+  val scalatestVersion    = "3.2.17"
+  val commonDomainVersion = "1.0.0"
 
   lazy val commonDomain = compileDependencies ++ testDependencies.map(_ % "test")
 
   lazy val root = compileDependencies ++ testDependencies
 
   private lazy val compileDependencies = Seq(
-    "uk.gov.hmrc"             %% "api-platform-common-domain"       % commonDomainVersion
+    "uk.gov.hmrc"             %% "api-platform-common-domain"          % commonDomainVersion
   )
 
   private lazy val testDependencies = Seq(
-    "com.vladsch.flexmark"     % "flexmark-all"                     % "0.62.2",
-    "org.mockito"             %% "mockito-scala-scalatest"          % "1.17.29",
-    "org.scalactic"           %% "scalactic"                        % scalatestVersion,
-    "org.scalatest"           %% "scalatest"                        % scalatestVersion,
-    "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures"  % commonDomainVersion,
+    "com.vladsch.flexmark"     % "flexmark-all"                        % "0.62.2",
+    "org.mockito"             %% "mockito-scala-scalatest"             % "1.17.29",
+    "org.scalactic"           %% "scalactic"                           % scalatestVersion,
+    "org.scalatest"           %% "scalatest"                           % scalatestVersion,
+    "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures" % commonDomainVersion,
     )
 }
